6eced586f18153b08e65285d61f2d8e5aba23d0e
Coder_WangChneyang <1351093262@qq.com>
2021-09-17 14:51:53 +0800

    探花后端所有代码以及资料已全部上传！

--- my-tanhua-sso/src/test/java/com/tanhua/sso/service/SendSms.java
+++ my-tanhua-sso/src/test/java/com/tanhua/sso/service/SendSms.java
@@ -11,8 +11,8 @@ import com.aliyuncs.profile.DefaultProfile;
 
 public class SendSms {
     public static void main(String[] args) {
-        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou",
-                "LTAI5tBWxUQ2ge6j2r2yXPg3", "jobKK1Z5fynt7GwyPNlxgia8WPR3kZ");
+        DefaultProfile profile = DefaultProfile.getProfile("",
+                "", "");
         IAcsClient client = new DefaultAcsClient(profile);
 
         CommonRequest request = new CommonRequest();
@@ -27,8 +27,6 @@ public class SendSms {
         request.putQueryParameter("TemplateParam", "{\"code\":\"123456\"}");//模板中变量替换
         try {
             CommonResponse response = client.getCommonResponse(request);
-
-            //{"Message":"OK","RequestId":"EC2D4C9A-0EAC-4213-BE45-CE6176E1DF23","BizId":"110903802851113360^0","Code":"OK"}
             System.out.println(response.getData());
         } catch (ServerException e) {
             e.printStackTrace();
